a36781a826631c46d37e757b40d34ab331deef21
Leonardo Rebelo <leonardorebelo02@gmail.com>
2024-11-20 18:30:50 -0300

    Update Main.java

--- SGL 0.1/Main.java
+++ SGL 0.1/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        SistemaLinks sistema = new SistemaLinks();
+        controleLinks sistema = new controleLinks();
         sistema.mostrarMenu();
     }
 }
